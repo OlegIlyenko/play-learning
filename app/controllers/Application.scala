12cd981dd207309978f95b33ce9c05cb73d86e87
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2013-08-05 00:32:17 +0200

    options method support

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,23 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.Play
 
 object Application extends Controller {
+
+  private val OptionMethods = List("GET", "POST", "PUT", "DELETE")
 
   def index = Action {
     Redirect(routes.Books.list)
   }
 
+  def options(path: String) = Action { implicit req =>
+    val routes = Play.current.routes.get
+    val supportedMethods = methods.filter(m => routes.handlerFor(req.copy(method = m)).isDefined)
+
+    if (supportedMethods.nonEmpty)
+      Ok(supportedMethods mkString "\n")
+    else
+      NotFound
+  }
 }
